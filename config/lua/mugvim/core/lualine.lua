79dba60e61ae8240f8649704f05a4d41936ddcc2
Long Huynh Huu <long@putputlaw.de>
2022-12-20 19:30:01 +0100

    lualine: globalstatus=true

--- config/lua/mugvim/core/lualine.lua
+++ config/lua/mugvim/core/lualine.lua
@@ -13,7 +13,7 @@ function M.setup()
             },
             ignore_focus = {},
             always_divide_middle = true,
-            globalstatus = false,
+            globalstatus = true,
             refresh = {
                 statusline = 1000,
                 tabline = 1000,
