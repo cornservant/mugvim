b344bee4555760a82ce8d51bece1fa8774890408
Long Huynh Huu <long@putputlaw.de>
2023-03-07 09:57:04 +0100

    fix which-key

--- config/lua/mugvim/core/which-key.lua
+++ config/lua/mugvim/core/which-key.lua
@@ -2,7 +2,6 @@ local M = {}
 
 local fs = require('mugvim.fs')
 local paths = require('mugvim.bootstrap')
-local wk = require('which-key')
 local userconfig = fs.join_paths(paths.config_dir, 'after', 'plugin', 'userconfig.lua')
 
 function M.edit_userconfig()
@@ -10,11 +9,11 @@ function M.edit_userconfig()
 end
 
 function M.setup()
-    wk.register({
+    require('which-key').register({
         c = { vim.cmd.bdelete, 'Close Buffer' },
         w = { vim.cmd.write, 'Write' },
         q = { vim.cmd.quit, 'Quit' },
-        ['+'] = {
+            ['+'] = {
             name = "Mugvim",
             c = { M.edit_userconfig, 'Edit User Configuration' },
             l = { function() require('lazy').home() end, 'Lazy' },
