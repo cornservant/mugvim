7b36aae078c86d3cfdc2937f442ec014aff571ae
Long Huynh Huu <long@putputlaw.de>
2023-05-21 16:42:24 +0200

    migrate to lsp-zero 2.x

--- config/lua/spec/lsp.lua
+++ config/lua/spec/lsp.lua
@@ -9,41 +9,38 @@ return {
     {
         'VonHeikemen/lsp-zero.nvim',
         lazy = false,
-        branch = 'v1.x',
+        branch = 'v2.x',
         dependencies = {
             -- LSP Support
             { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
+            {
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
             { 'williamboman/mason-lspconfig.nvim' },
 
             -- Autocompletion
             { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-nvim-lua' },
+            { 'saadparwaiz1/cmp_luasnip' },
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
+            -- { 'L3MON4D3/LuaSnip' },
+            -- { 'rafamadriz/friendly-snippets' },
 
             -- LSP lines
             { 'ErichDonGubler/lsp_lines.nvim' },
         },
         config = function()
-            local lsp = require('lsp-zero')
-            local wk = require('which-key')
+            local lsp = require('lsp-zero').preset({
+                set_sources = 'recommended',
+            })
 
-            wk.register({
-                l = {
-                    name = "LSP",
-                    ["="] = { name = "Format" }
-                }
-            }, { prefix = "<leader>" })
-
-
-            lsp.preset('recommended')
             lsp.ensure_installed({
                 'tsserver',
                 'eslint',
@@ -51,36 +48,16 @@ return {
                 'jdtls',
             })
 
-
-            local cmp = require('cmp')
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
-            local cmp_mappings = lsp.defaults.cmp_mappings({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            })
-
-            -- disable completion with tab
-            -- this helps with copilot setup
-            cmp_mappings['<Tab>'] = nil
-            cmp_mappings['<S-Tab>'] = nil
-
-            lsp.setup_nvim_cmp({
-                mapping = cmp_mappings
-            })
-
-            lsp.set_preferences({
-                suggest_lsp_servers = false,
-                sign_icons = {
-                    error = '✘',
-                    warn = '▲',
-                    hint = '⚑',
-                    info = '»',
-                }
+            lsp.set_sign_icons({
+                error = '✘',
+                warn = '▲',
+                hint = '⚑',
+                info = '»',
             })
 
             lsp.on_attach(function(client, bufnr)
+                lsp.default_keymaps({buffer = bufnr})
+
                 local opts = { buffer = bufnr, remap = false }
 
                 if client.name == "eslint" then
@@ -94,7 +71,7 @@ return {
                 vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
                 vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 
-                wk.register({
+                require('which-key').register({
                     l = {
                         name = 'LSP',
                         a = { vim.lsp.buf.code_action, "Code Action" },
@@ -109,6 +86,14 @@ return {
             end)
 
             lsp.setup()
+
+            local cmp = require('cmp')
+
+            cmp.setup({
+                mapping = {
+                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+                }
+            })
 
             vim.diagnostic.config({
                 virtual_text = true,
