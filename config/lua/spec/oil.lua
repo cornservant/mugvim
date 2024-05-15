7f010493c229271ee0bee7f53f0a2ff12070ba88
Long Huynh Huu <long@putputlaw.de>
2024-05-15 18:55:49 +0200

    feat(oil) trash feature

--- config/lua/spec/oil.lua
+++ config/lua/spec/oil.lua
@@ -5,9 +5,14 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("oil").setup({
+            view_options = {
+                show_hidden = true,
+            },
         })
     end,
+    delete_to_trash = true,
     keys = {
-        { "<leader>e", function() require("oil").open() end, desc = "File Browser" },
+        { "<leader>e",  function() require("oil").open() end,                          desc = "File Browser" },
+        { "<leader>ft", function() require("oil.actions").toggle_trash.callback() end, desc = "Browse Trash" },
     },
 }
