663f6979f8d9c90a1b95ef1d43008ddcb47e83d2
Long Huynh Huu <long@putputlaw.de>
2024-10-10 01:53:55 +0200

    feat(treesitter) c3 syntax highlighting

--- config/lua/spec/treesitter.lua
+++ config/lua/spec/treesitter.lua
@@ -5,6 +5,23 @@ return {
         ts_update()
     end,
     config = function()
+        vim.filetype.add({
+            extension = {
+                c3 = "c3",
+                c3i = "c3",
+                c3t = "c3",
+            },
+        })
+
+        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+        parser_config.c3 = {
+            install_info = {
+                url = "https://github.com/c3lang/tree-sitter-c3",
+                files = { "src/parser.c", "src/scanner.c" },
+                branch = "main",
+            },
+        }
+
         require 'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all"
             ensure_installed = {
