618b80100360d663b52934c73b5711e1346b7416
Long Huynh Huu <long@putputlaw.de>
2024-10-10 01:48:56 +0200

    fix(which-key) adapt new format

--- config/lua/spec/which-key.lua
+++ config/lua/spec/which-key.lua
@@ -1,33 +1,18 @@
 return {
     'folke/which-key.nvim',
     config = function()
-        require('which-key').register({
-            b = {
-                name = "Buffer"
-            },
-            f = {
-                name = "File"
-            },
-            g = {
-                name = "Git",
-            },
-            s = {
-                name = "Search",
-            },
-            t = {
-                name = "Telescope",
-                h = {
-                    name = "History",
-                },
-            },
-            ['+'] = {
-                name = "Mugvim",
-                -- c = { M.edit_userconfig, 'Edit User Configuration' },
-                l = { function() require('lazy').home() end, 'Lazy' },
-            },
-            c = { vim.cmd.bdelete, 'Close Buffer' },
-            w = { vim.cmd.write, 'Write' },
-            q = { vim.cmd.quit, 'Quit' },
-        }, { prefix = '<leader>' })
+        require('which-key').add({
+            { "<leader>+",  group = "Mugvim" },
+            { "<leader>+l", function() require('lazy').home() end, desc = "Lazy" },
+            { "<leader>b",  group = "Buffer" },
+            { "<leader>c",  vim.cmd.bdelete,                       desc = "Close Buffer" },
+            { "<leader>f",  group = "File" },
+            { "<leader>g",  group = "Git" },
+            { "<leader>q",  vim.cmd.quit,                          desc = "Quit" },
+            { "<leader>s",  group = "Search" },
+            { "<leader>t",  group = "Telescope" },
+            { "<leader>th", group = "History" },
+            { "<leader>w",  vim.cmd.write,                         desc = "Write" },
+        })
     end,
 }
