f63a4ec60b14ee43d4ef462276ffbdb0cb61dcd2
Long Huynh Huu <long@putputlaw.de>
2024-10-15 22:17:47 +0200

    snippety snip

--- config/lua/spec/lsp.lua
+++ config/lua/spec/lsp.lua
@@ -52,9 +52,9 @@ return {
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
-            'hrsh7th/vim-vsnip',
-            'hrsh7th/cmp-vsnip',
             'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
         },
         config = function()
             -- Set up nvim-cmp.
@@ -63,7 +63,7 @@ return {
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body)
+                        require("luasnip").lsp_expand(args.body)
                     end,
                 },
                 window = {
@@ -75,11 +75,11 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
-                    { name = 'vsnip' },
+                    { name = 'luasnip' },
                 }, {
                     { name = 'buffer' },
                 })
@@ -198,14 +198,6 @@ return {
                         { "<leader>lw", vim.lsp.buf.workspace_symbol,                       buffer = ev.buf, desc = "Workspace Symbols" },
                     })
                 end,
-            })
-
-            local cmp = require('cmp')
-
-            cmp.setup({
-                mapping = {
-                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-                }
             })
 
             vim.diagnostic.config({
---
+++ config/lua/spec/luasnip.lua
@@ -0,0 +1,30 @@
+local mugvimPaths = require("mugvim.bootstrap")
+
+return {
+    "L3MON4D3/LuaSnip",
+    lazy = false,
+    version = "v2.*",
+    build = "make install_jsregexp",
+    config = function()
+        local ls = require("luasnip")
+
+        local expand_or_jump = function(dir)
+            local key
+            if dir == 1 then
+                key = "<Tab>"
+            else
+                key = "<S-Tab>"
+            end
+            if ls.jumpable(dir) then
+                ls.jump(dir)
+            else
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", false)
+            end
+        end
+
+        vim.keymap.set({"i", "s"}, "<Tab>", function() expand_or_jump(1) end, { remap = false })
+        vim.keymap.set({"i", "s"}, "<S-Tab>", function() expand_or_jump(-1) end, { remap = false })
+
+        require("luasnip.loaders.from_snipmate").lazy_load({ paths = mugvimPaths.base_dir .. "/snippets" })
+    end,
+}
