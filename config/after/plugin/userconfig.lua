978b0511346e3344c49d91b1d65ed2d06041e30a
Long Huynh Huu <long@putputlaw.de>
2025-01-08 23:29:01 +0100

    userconfig

--- config/after/plugin/userconfig.lua
+++ config/after/plugin/userconfig.lua
@@ -1,6 +1,20 @@
-require("mugvim.colors").colors("tokyonight")
--- vim.cmd(":TransparentDisable")
+-- local themes = { "catppuccin-mocha", "tokyonight-moon", "rose-pine-moon", "doom-ayu-mirage" }
+-- local randomTheme = themes[math.random(1, #themes)]
+require("mugvim.colors").colors("tokyonight-moon")
+vim.cmd(":TransparentEnable")
+vim.o.cursorline = false
+vim.diagnostic.config({ virtual_lines = false })
+vim.g.indent_blankline_enabled = false
 
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = "*.adoc",
+    group = vim.api.nvim_create_augroup("asciidoc_to_html", { clear = true }),
+    callback = function(ctx)
+        local cmd = "silent !asciidoctor " .. ctx.file
+        vim.cmd(cmd)
+    end,
+})
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = "*",
@@ -24,3 +38,12 @@ vim.api.nvim_create_autocmd("FileType", {
         set nobuflisted
     ]],
 })
+--
+-- vim.api.nvim_create_autocmd({ "VimLeave" }, {
+--     pattern = "home.nix",
+--     group = vim.api.nvim_create_augroup("home-manager", { clear = true }),
+--     callback = function(ctx)
+--         local cmd = "!env NIXPKGS_ALLOW_UNFREE=1 home-manager switch"
+--         vim.cmd(cmd)
+--     end,
+-- })
