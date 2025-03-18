f4bd7a5ea215aca9ffe104220a7e99f8b2632f58
Long Huynh Huu <long@putputlaw.de>
2025-03-18 12:17:03 +0100

    feat: made my own buffer editor plugin

--- runtime/lua/plugins/bufferline.lua
+++ runtime/lua/plugins/bufferline.lua
@@ -1,6 +1,15 @@
 return {
-    'akinsho/bufferline.nvim',
-    config = function()
-        require('bufferline').setup({})
-    end,
+    {
+        'akinsho/bufferline.nvim',
+        opts = {},
+    },
+    {
+        'https://git.loporrit.de/long/bufferline-editor.nvim',
+        dependencies = { 'akinsho/bufferline.nvim' },
+        opts = {},
+        keys = {
+            { "<c-e>", function() require("bufferline-editor").editor_toggle() end, desc = "Edit Buffers" },
+            { "<leader>be", function() require("bufferline-editor").editor_toggle() end, desc = "Edit Buffers" },
+        }
+    },
 }
