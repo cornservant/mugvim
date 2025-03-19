bf8f532111150576d81937fde5c749dddb14aef1
Long Huynh Huu <long@putputlaw.de>
2025-03-20 00:27:39 +0100

    feat(mugvim): custom bufferline editor size

--- runtime/lua/plugins/bufferline.lua
+++ runtime/lua/plugins/bufferline.lua
@@ -7,7 +7,10 @@ return {
         'https://git.loporrit.de/long/bufferline-editor.nvim',
         dependencies = { 'akinsho/bufferline.nvim' },
         ---@type bufferline-editor.config
-        opts = {},
+        opts = {
+            max_width = 80,
+            max_height = 24,
+        },
         keys = {
             { "<c-e>", function() require("bufferline-editor").editor_toggle() end, desc = "Edit Buffers" },
             { "<leader>be", function() require("bufferline-editor").editor_toggle() end, desc = "Edit Buffers" },
