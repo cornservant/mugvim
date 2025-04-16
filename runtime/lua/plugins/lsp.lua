2bd10152d5ec55ffe583cb63a9abf20f93c04e66
Long Huynh Huu <long@putputlaw.de>
2025-04-16 06:32:58 +0200

    refactor(lualine): move lsp-status dependency to proper location

--- runtime/lua/plugins/lsp.lua
+++ runtime/lua/plugins/lsp.lua
@@ -3,9 +3,6 @@ return {
         'folke/neodev.nvim'
     },
     {
-        'nvim-lua/lsp-status.nvim'
-    },
-    {
         "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     },
     {
--- runtime/lua/plugins/lualine.lua
+++ runtime/lua/plugins/lualine.lua
@@ -41,6 +41,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = {
         'nvim-tree/nvim-web-devicons',
+        'nvim-lua/lsp-status.nvim'
     },
     config = function()
         require('lualine').setup({
