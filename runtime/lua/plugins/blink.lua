89ac5630737c45186320b731f5f483a340db2646
Long Huynh Huu <long@putputlaw.de>
2025-04-16 23:59:50 +0200

    fix(blink): don't override digraph

--- runtime/lua/plugins/blink.lua
+++ runtime/lua/plugins/blink.lua
@@ -21,7 +21,25 @@ return {
         -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
         -- see the "default configuration" section below for full documentation on how to define
         -- your own keymap.
-        keymap = { preset = 'enter' },
+        -- NOTE: I am using a modified 'enter' preset
+        keymap = {
+            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            ['<C-e>'] = { 'hide', 'fallback' },
+            ['<CR>'] = { 'accept', 'fallback' },
+
+            ['<Tab>'] = { 'snippet_forward', 'fallback' },
+            ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+            ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+            ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        },
         -- completion = {
         --   menu = { auto_show = function(ctx) return ctx.mode ~= 'cmdline' end }
         -- },
