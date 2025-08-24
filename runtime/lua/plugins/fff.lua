cd654b32a1eaf2810f47aa4b3bc7b0a4b3a72082
Long Huynh Huu <long@putputlaw.de>
2025-08-24 21:59:08 +0200

    feat: remove fff

--- runtime/lua/plugins/fff.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'dmtrKovalenko/fff.nvim',
-    build = "cargo build --relase",
-    opts = {},
-    keys = {
-        { "<leader>F", function() require("fff").find_files() end, desc = "Open file picker", },
-    },
-}
