5957b7f64435e921e7c882d3007e0b34b705f016
Long Huynh Huu <long@putputlaw.de>
2025-11-02 18:25:45 +0100

    feat: binding for outline

--- runtime/lua/plugins/symbols-outline.lua
+++ runtime/lua/plugins/symbols-outline.lua
@@ -3,4 +3,7 @@ return {
     config = function()
         require('outline').setup()
     end,
+    keys = {
+        { "<leader>o", function() require 'outline'.open() end, desc = "Open Outline" }
+    },
 }
