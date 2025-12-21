d75e7edbb312d566cfc6a980ac4469e9ca20a95f
Long Huynh Huu <long@putputlaw.de>
2025-12-21 13:48:52 +0100

    feat: update bufferline editor

--- runtime/lua/plugins/bufferline.lua
+++ runtime/lua/plugins/bufferline.lua
@@ -5,14 +5,17 @@ return {
     },
     {
         'https://git.loporrit.de/long/bufferline-editor.nvim',
-        dependencies = { 'akinsho/bufferline.nvim' },
+        dependencies = {
+            'akinsho/bufferline.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
         ---@type bufferline-editor.config
         opts = {
             max_width = 80,
             max_height = 24,
         },
         keys = {
-            { "<c-e>", function() require("bufferline-editor").editor_toggle() end, desc = "Edit Buffers" },
+            { "<c-e>",      function() require("bufferline-editor").editor_toggle() end, desc = "Edit Buffers" },
             { "<leader>be", function() require("bufferline-editor").editor_toggle() end, desc = "Edit Buffers" },
         }
     },
