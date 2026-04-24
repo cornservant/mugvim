f80dfe222c905a02046888626a2e30718678a016
Long Huynh Huu <long@putputlaw.de>
2026-04-24 16:25:54 +0200

    perf(util): time measurement function

--- runtime/lua/mugvim/util.lua
+++ runtime/lua/mugvim/util.lua
@@ -9,4 +9,15 @@ function M:load_on_ft(name, ft_pattern, callback)
     })
 end
 
+function M:measure_time_ms(action)
+    local start = vim.uv.hrtime()
+    action()
+    return (vim.uv.hrtime() - start) / 1e6
+end
+
+function M:measure_and_print(name, action)
+    local duration_ms = M:measure_time_ms(action)
+    print(string.format("%s: %.4f ms", name, duration_ms))
+end
+
 return M
