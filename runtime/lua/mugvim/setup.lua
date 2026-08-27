1ed21ae67355e6da431e10ab3104fb8daebc0998
Long Huynh Huu <long@putputlaw.de>
2026-08-28 00:06:15 +0200

    feat: notify when changing format-on-write setting

--- runtime/lua/mugvim/setup.lua
+++ runtime/lua/mugvim/setup.lua
@@ -167,6 +167,11 @@ end
 function M:base_commands()
     local function toggle_format_on_write()
         vim.g.mugvim_autoformat = not vim.g.mugvim_autoformat
+        if vim.g.mugvim_autoformat then
+            vim.notify("format on write: on", vim.log.levels.INFO)
+        else
+            vim.notify("format on write: off", vim.log.levels.INFO)
+        end
     end
     vim.api.nvim_create_user_command("MugvimEditUserConfig", function() require("mugvim"):edit_user_config() end, {})
     vim.api.nvim_create_user_command("MugvimVersion", function() print(require("mugvim"):version()) end, {})
