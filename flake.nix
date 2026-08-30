d25b6bae2a7692445edad899a809c82a57dda8d8
Long Huynh Huu <long@putputlaw.de>
2026-08-30 08:41:37 +0200

    refactor(flake): drop flake-parts dependency

--- flake.nix
+++ flake.nix
@@ -1,31 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/f4f698677b11021a8f84f452e23ae9ef2427bec3.tar.gz";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
   outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem =
-        { pkgs, ... }:
-        rec {
-          formatter = pkgs.nixfmt;
-
-          packages = rec {
-            default = mugvim;
-            mugvim = pkgs.callPackage ./package.nix { };
-          };
-
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              packages.mugvim
-              neovim
-              tree-sitter
-              zig
-              nurl
-            ];
-          };
-        };
+    { nixpkgs, ... }:
+    let
+      eachSupportedSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      eachPkgs = f: eachSupportedSystem (system: f (nixpkgs.legacyPackages.${system}));
+    in
+    {
+      formatter = eachPkgs (pkgs: pkgs.nixfmt);
+      packages = eachPkgs (pkgs: rec {
+        default = mugvim;
+        mugvim = pkgs.callPackage ./package.nix { };
+      });
+      devShells = eachPkgs (
+        pkgs:
+        pkgs.mkShell {
+          packages = with pkgs; [
+            packages.mugvim
+            neovim
+            tree-sitter
+            zig
+            nurl
+          ];
+        }
+      );
     };
 }
